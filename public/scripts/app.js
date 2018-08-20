0880f91072acdca303173908f79eb0dc65cf732b
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-08-20 22:15:28 +0000

    fetching tweets using get request to the server  completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,53 +11,6 @@ $(document).ready(function() {
 
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 
 
 //part of the function used from https://gist.github.com/flangofas/714f401b63a1c3d84aaa
@@ -125,7 +78,28 @@ function renderTweets(tweets) {
   });
 }
 
-renderTweets(data);
+function loadTweets() {
+  //load tweets
+  $.get("/tweets", function(jsonTweets){
+    renderTweets(jsonTweets);  
+  });
+}
+
+
+
+
+loadTweets();
+
+
+
+
+$("#tweetSubmitButton").click(function(event){
+  event.preventDefault();
+  let $inputTweetText = $(".tweetInput").serialize();
+  $.post("/tweets", $inputTweetText, function () {
+    
+  })
+})
 
 });
 
